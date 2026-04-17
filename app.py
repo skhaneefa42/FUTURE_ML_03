7246344dc5f431eb86443d756de14d3f3f5bd74e
skhaneefa42 <skhaneefa42@gmail.com>
2026-04-17 19:12:20 +0530

    removed matplotlib

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ All features: Resume ranking, skill extraction, skill gap analysis, visualizatio
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
