258b41717094c0f03d9582237cc69f3199fcac9b
skhaneefa42 <skhaneefa42@gmail.com>
2026-04-17 18:34:58 +0530

    added upload feature

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-
-
 import streamlit as st
 
 st.title("Resume Screening System")
 
-st.write("App is running successfully 🎉")
+uploaded_file = st.file_uploader("Upload your resume", type=["pdf", "txt"])
+
+if uploaded_file is not None:
+    st.success("File uploaded successfully 🎉")
+    st.write("File name:", uploaded_file.name)
